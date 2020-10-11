239b1a3842797b9ab741a89c8314b8fbd3e65154
Pavlouha <sensei_khan@rambler.ru>
2020-10-11 21:31:04 +0300

    deleted branch

--- HandControlMQTTServer/src/main/kotlin/EndpointInformationModel.kt
+++
@@ -1,7 +0,0 @@
-import io.vertx.mqtt.MqttEndpoint
-import io.vertx.mqtt.MqttTopicSubscription
-
-class EndpointInformationModel(mqttEndpoint : MqttEndpoint) {
-    val endpoint : MqttEndpoint = mqttEndpoint
-    val topics : MutableList<MqttTopicSubscription> = ArrayList()
-}
