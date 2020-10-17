28c143f11a92208394218a770d71c865fbc584fd
mishkamashka <anisimova.mariia.mm@gmail.com>
2020-10-17 16:08:28 +0300

    spring data domain, add session class

--- server/src/main/kotlin/com/handcontrol/server/dao/SessionRepository.kt
+++ server/src/main/kotlin/com/handcontrol/server/dao/SessionRepository.kt
@@ -2,5 +2,7 @@ package com.handcontrol.server.dao
 
 import com.handcontrol.server.model.Session
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SessionRepository: CrudRepository<Session, String>
